2e7bc09a3a4e04a1402403abee2025c202480b56
ST10517811 <ST10517811@myemeris.edu.za>
2026-02-25 14:18:15 +0200

    commit version one

---
+++ src/test1/Test1.java
@@ -0,0 +1,33 @@
+/**
+ * the flowing project is stop and go signs on a ground 
+ * the purpose of the following activity is to learn the \ commands  
+ */
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test1;
+
+/**
+ *
+ * @author User
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // the fllowing line of code is the structue of the signs and the wording
+        System.out.println("hello");
+        System.out.println("\t   ------  \t\t\t   ------   \n\t -        - \t\t\t -        - \n\t-   STOP   - \t\t\t-    GO    -\n\t -        - \t\t\t -        - \n\t   ------  \t\t\t   ------  \n\t     --    \t\t\t     --    \n\t     --    \t\t\t     --    \n\t     --    \t\t\t     --    \n\t     --    \t\t\t     --    \n\t    ----\t\t\t    ----\n\t ----------\t\t\t ----------");
+            
+        // the following lines of code is the base/ ground of the signs flovers were also added, the head is "@" and the stem is "|"
+        System.out.println("|-------@-----------@-----------@-------------@--------|");
+        System.out.println("|-------|-----------|-----------|-------------|--------|");
+        System.out.println("|-------|-----------|-----------|-------------|--------|");
+    }
+    
+}
